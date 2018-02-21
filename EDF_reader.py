7784bb53c860468755ba10746214a37e21a06c09
lukerollings <33325701+lukerollings@users.noreply.github.com>
2018-02-21 18:10:43 +0000

    Add files via upload
    Now saves each frame as a jpeg

--- EDF_reader.py
+++ EDF_reader.py
@@ -10,12 +10,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import fabio
 import glob
+from PIL import Image
 
 os.chdir('C://Users//mbgnwlr2//Documents//PhD_SynchrotronStuff//Winter18_beamtime_data//radio//Al_SiC_1D_0')
 
 images = glob.glob('Al_SiC_1D_0_*.edf')
 
+
+I = 0
+
 for i in images:
-    img = fabio.open(str(i))
+    edf = fabio.open(str(i))
+    img = edf.data
     plt.figure(figsize=(8,4))
-    plt.imshow(img.data)
+    plt.imsave('Al_SiC_1D_0_000'+str(I)+'.jpg', img)
+
+    I = I+1
