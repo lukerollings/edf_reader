8bd58703750b041b9f5e95ea92f98c4deac2502d
lukerollings <33325701+lukerollings@users.noreply.github.com>
2018-02-28 19:35:12 +0000

    Add files via upload
    Now stitches images into single image along the length of the gauge

--- EDF_reader.py
+++ EDF_reader.py
@@ -17,22 +17,35 @@ os.chdir('C://Users//mbgnwlr2//Documents//PhD_SynchrotronStuff//Winter18_beamtim
 images = glob.glob('Al_SiC_1D_0_*[0-4].edf')
 
 light = fabio.open('Al_SiC_1D_0_0005.edf')
-background = light.data
+bg = light.data
 dark = fabio.open('Al_SiC_1D_0_0006.edf')
 darkfield = dark.data
-full = np.full_like(background, np.amax(background))
+
+base = np.empty([5682, 1780])
 
 I = 0
 
 for i in images:
     edf = fabio.open(str(i))
     img = edf.data
-    new = np.divide(img, background, dtype = np.float32)
-    final = exposure.rescale_intensity(new, (0.0510, 1.7))
+    new = np.divide(img, bg, dtype = np.float32)
+    normalised = exposure.rescale_intensity(new, (0.05, 1.7))
+    final = normalised[365:1795, 390:2170]
     plt.figure(figsize=(8,4))
-    plt.imshow(new)
+#    plt.imshow(final)
 #    plt.imsave('Al_SiC_1D_0_000'+str(I)+'.jpg', new)
-
-    I = I+1
     
-#images = glob.glob('Al_SiC_1D_0_*.jpg')
+    
+
+    
+    
+    x = 0
+    
+    
+    while x < final.shape[0]:
+        base[x+I, :] = final[x, :]
+        x = x+1
+        
+    I = I+1063
+    
+plt.imshow(base)
